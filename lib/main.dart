693dde75512797d8096f127ca830dced2b310d56
monfadev <mfaauzii@gmail.com>
2022-05-19 22:21:11 +0700

    Update unused code

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:mresponsive/core/config/config.dart';
 import 'package:mresponsive/ui/constant/constant.dart';
 import 'package:mresponsive/ui/router/router_generator.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const Root());
@@ -21,16 +20,6 @@ class Root extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: whiteColor,
         fontFamily: fontName,
-      ),
-      builder: (context, widget) => ResponsiveWrapper.builder(
-        BouncingScrollWrapper.builder(context, widget!),
-        breakpoints: [
-          const ResponsiveBreakpoint.resize(450, name: MOBILE),
-          const ResponsiveBreakpoint.resize(800, name: TABLET),
-          const ResponsiveBreakpoint.resize(1000, name: TABLET),
-          const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
-          const ResponsiveBreakpoint.resize(2460, name: "4K"),
-        ],
       ),
     );
   }
